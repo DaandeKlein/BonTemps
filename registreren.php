52cbe84c2105f6de938de33219bcb2bbb1581a96
DaandeKlein <Daan.Klein@rocdeleijgraaf.nl>
2022-10-04 12:23:15 +0200

    max input aangepast zodat je geen sql error kan krijgen

--- registreren.php
+++ registreren.php
@@ -20,27 +20,27 @@
         <form action="Submit.php" method="post" class="align-content-center">
             <div class="mb-3">
                 <label for="VoorbeeldNaamInput" class="form-label">Naam:</label>
-                <input type="text" name="Naam" class="form-control" id="VoorbeeldNaamInput" placeholder="Voor- en Achternaam" required>
+                <input type="text" name="Naam" class="form-control" id="VoorbeeldNaamInput" placeholder="Voor- en Achternaam" maxlength="255" required>
             </div>
             <div class="mb-3">
                 <label for="VoorbeeldEmailInput" class="form-label">E-mailadres:</label>
-                <input type="email" name="Email" class="form-control" id="VoorbeeldEmailInput" aria-describedby="emailHelp" placeholder="E-mailadres" required>
+                <input type="email" name="Email" class="form-control" id="VoorbeeldEmailInput" aria-describedby="emailHelp" placeholder="E-mailadres" maxlength="255" required>
             </div>
             <div class="mb-3">
                 <label for="VoorbeeldAdresInput" class="form-label">Adres:</label>
-                <input type="text" name="Adres" class="form-control" id="VoorbeeldAdresInput" placeholder="Adres" required>
+                <input type="text" name="Adres" class="form-control" id="VoorbeeldAdresInput" placeholder="Adres" maxlength="255" required>
             </div>
             <div class="mb-3">
                 <label for="VoorbeeldPostcodeInput" class="form-label">Postcode:</label>
-                <input type="text" name="Postcode" class="form-control" id="VoorbeeldPostcodeInput" placeholder="0000AA" required>
+                <input type="text" name="Postcode" class="form-control" id="VoorbeeldPostcodeInput" placeholder="0000AA" maxlength="6" required>
             </div>
             <div class="mb-3">
                 <label for="VoorbeeldTelefoonInput" class="form-label">Telefoonnummer:</label>
-                <input type="text" name="Telefoon" class="form-control" id="VoorbeeldTelefoonInput" placeholder="00-00000000" required>
+                <input type="text" name="Telefoon" class="form-control" id="VoorbeeldTelefoonInput" placeholder="00-00000000" maxlength="10" required>
             </div>
             <div class="mb-3">
                 <label for="VoorbeeldWachtwoordInput" class="form-label">Wachtwoord</label>
-                <input type="text" name="Wachtwoord" class="form-control" id="VoorbeeldWachtwoordInput" required>
+                <input type="text" name="Wachtwoord" class="form-control" id="VoorbeeldWachtwoordInput" maxlength="255" required>
             </div>
             <button type="submit" name="registreren" class="btn btn-primary">Registreren</button>
             <?php 
